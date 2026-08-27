0125710ecb0b4a94b8120b095846d6f161515c73
Tobbi <tolusamuel040@gmail>
2026-08-27 09:55:39 +0100

    updated

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, unique: true, lowercase: true, trim: true },
-  passwordHash: { type: String, required: true },
+  passwordHash: { type: String, default: '' },
   role: { type: String, enum: ['student', 'admin'], default: 'student' },
   subjects: { type: [String], default: [] },
   createdAt: { type: Date, default: Date.now }
